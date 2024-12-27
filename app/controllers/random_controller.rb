7b0e270d950eb301d8930ce1f69d7924978131d5
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-12-27 05:30:18 +0000

    all routes worked

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,1 +1,12 @@
+class RandomController < ApplicationController
+  def new
+    render({:template => "pages_templates/random"})
+  end
 
+  def get_result
+    @min = params.fetch("user_min").to_f
+    @max = params.fetch("user_max").to_f
+    @result = rand(@min..@max)
+    render({:template => "pages_templates/random_result"})
+  end
+end
